d2a1bac035af2b81ed846950d8c04f717e33489a
Muhammad Ayaz <ayazmughal806@gmail.com>
2026-03-12 15:51:06 +0500

    DB & Server Setup

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb://localhost:27017';
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('Connected to Database Successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectToMongo;
