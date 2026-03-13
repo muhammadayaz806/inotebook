76e6802d3d752024f05de6d592f8584d8a99dd12
Muhammad Ayaz <ayazmughal806@gmail.com>
2026-03-13 16:14:43 +0500

    Hashed Passwords using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'iNoteBookSecretKey';
+
+// Route 1: Create a User using: POST "/api/auth/createuser". No login required
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Name must be at least 3 characters long").isLength({
       min: 3,
@@ -14,19 +19,48 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async(req, res) => {
+    // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+
+    // Check whether the user with this email exists already
+    try {
+    let user = await User.findOne({ email: req.body.email});
+    if (user) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
+    // Password Hashing
+    let salt = await bcrypt.genSalt(10);
+    let secPass = await bcrypt.hash(req.body.password, salt);
+
+    user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
-    }).then(user => res.json(user)).catch(err => {
-      console.log(err);
-      res.status(500).json({ error: "Email already exists" });
-    });
+      password: secPass,
+    })
+
+    // .then(user => res.json(user)).catch(err => {
+    //   console.log(err);
+    //   res.status(500).json({ error: "Email already exists" });
+    // });
+    // res.json(user);
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authToken = jwt.sign(data, JWT_SECRET);
+    res.json({ authToken });
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
   },
 );
 
